00c7882826092d3de2d3cdbfd1aa5b10516eaa06
Paulo Cunha <pcunha00@hotmail.com>
2021-02-10 16:47:48 +0100

    implement player line

--- srcs/render_player.c
+++ srcs/render_player.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 15:37:26 by user42            #+#    #+#             */
-/*   Updated: 2021/02/10 15:41:32 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/10 16:30:48 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 void	render_player(t_vars *vars)
 {
 	t_input_rect	r;
+	t_pto p0;
+	t_pto p1;
 
 	r.pto_sup_esq.x = vars->player.x - vars->player.radius;
 	r.pto_sup_esq.y = vars->player.y - vars->player.radius;
@@ -24,4 +26,10 @@ void	render_player(t_vars *vars)
 	r.borda = 0;
 	r.cor_borda = GREY;
 	g_plot_rect_img(vars, r);
+	p0.x = vars->player.x;
+	p0.y = vars->player.y;
+	p1.x = p0.x + 30 * cos(vars->player.angle);
+	p1.y = p0.y + 30 * sin(vars->player.angle);
+	g_plot_line_img(vars, p0, p1, RED);
+
 }
