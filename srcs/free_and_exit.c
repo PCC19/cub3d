765b5b21524febb1d930ce03af774ecab66719e6
Paulo Cunha <pcunha00@hotmail.com>
2021-02-17 22:50:25 +0100

    INICIALIZANDO FINALIZACAO

--- srcs/free_and_exit.c
+++ srcs/free_and_exit.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 22:08:45 by user42            #+#    #+#             */
-/*   Updated: 2021/02/17 22:28:02 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/17 22:44:47 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,19 @@ int		close_program()
 	return (0);
 }
 
-int		free_and_exit(t_vars *vars)
+int		free_and_exit(t_vars *v)
 {
-	printf("%d\n",vars->player.x);
-	printf(" TA DA !!!!!\n");
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		mlx_destroy_image(v->mlx, v->tex[i].id);
+		i++;
+	}
+	mlx_destroy_image(v->mlx, v->t.id);
+	mlx_destroy_window(v->mlx, v->win);
+	free(v->mlx);
+	printf(" PLAY AGAIN !!!!!\n");
 	return (close_program());
 }
-
--- srcs/k_hooks.c
+++ srcs/k_hooks.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 22:23:35 by user42            #+#    #+#             */
-/*   Updated: 2021/02/16 21:43:02 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/17 22:30:07 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int		key_hook(int keycode, t_vars *vars)
 {
-	printf("keycode: %d\n", keycode);
 	if (keycode == RIGHT)
 		turn_right(vars);
 	if (keycode == LEFT)
@@ -27,7 +26,8 @@ int		key_hook(int keycode, t_vars *vars)
 		move_left(vars);
 	if (keycode == DKEY)
 		move_right(vars);
-	//update(vars);
+	if (keycode == ESCKEY)
+		free_and_exit(vars);
 	draw(vars);
 	return (0);
 }
