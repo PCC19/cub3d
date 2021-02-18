499b409ece937c183a64e7c591bd1c29888d413c
Paulo Cunha <pcunha00@hotmail.com>
2021-02-18 16:04:35 +0100

    fixed p_parse_argumentos

--- srcs/p_parse_argumentos.c
+++ srcs/p_parse_argumentos.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 23:34:33 by user42            #+#    #+#             */
-/*   Updated: 2021/02/16 23:02:49 by user42           ###   ########.fr       */
+/*   Updated: 2021/02/18 16:04:19 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	p_parse_argumentos(int argc, char *argv[])
 	char	*substr;
 
 	codigo = 0;
-	if (argc < 1 || argc > 3)
+	if (argc < 2 || argc > 3)
 		codigo = WRONG_ARGS;
-	if (argc > 0)
+	if (argc == 2)
 	{
 		str = ft_strtrim(argv[1], " ./");
 		substr = ft_strrchr(str, '.');
